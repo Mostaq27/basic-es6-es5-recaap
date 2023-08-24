05c3a7bdac21fcb11b8c402bb9e6104c96aef144
Mostaq27 <mostaq99muzahid@gmail.com>
2023-08-25 00:41:49 +0600

    second commit

--- destructure.js
+++ destructure.js
@@ -7,7 +7,7 @@ const actor ={
 // if right side is an object left side of destrutring will be object as well
 // use property name as a variable that contains the property value
 
-const {phone} = actor 
+const {phone, age: boyos} = actor 
 
 // const phone =actor.phone;
 // const name = actor.name;
@@ -16,4 +16,19 @@ console.log(phone)
 console.log(phone)
 console.log(phone)
 // console.log(age)
+// console.log(age)
+console.log(boyos)
+console.log(boyos)
+console.log(boyos)
 // console.log(name)
+
+// array destructuring
+const Numbers = [45, 99]
+const [first, second] = Numbers;
+const [x, y] = [12, 66];
+// advanced
+function doubleThem(a, b){
+    return[a*2, b*2]
+}
+const [prothom, ditiyo] = doubleThem(6, 9);
+console.log(prothom,ditiyo)
---
+++ keys.js
@@ -0,0 +1,38 @@
+const glass ={
+    name:'glass',
+    color:'golden',
+    price:'12',
+    isCleaned:'true'
+};
+
+console.log(glass);
+// all properties name 
+const keys = Object.keys(glass);
+// ['name', 'color', 'price', 'isCleaned']
+
+// all properties values
+const values = Object.values(glass);
+// [ 'glass', 'golden', '12', 'true' ]
+// console.log(values)
+// console.log(keys)
+
+
+const entries = Object.entries(glass);
+// console.log(entries)
+// Array of Array , two dimensional Array
+// [
+//     [ 'name', 'glass' ],
+//     [ 'color', 'golden' ],
+//     [ 'price', '12' ],
+//     [ 'isCleaned', 'true' ]
+//   ]
+
+// delete glass.isCleaned;
+// console.log(glass)
+const {isCleaned, ...shortGlass} = glass
+// console.log(shortGlass)
+
+// freeze
+// Object.freeze(glass);
+    glass.source = 'Bangladesh'
+    console.log(glass)
